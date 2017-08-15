57a71fdf4fd567b13759ef1baad165f091ecf569
xfyangl <xfyangl@isoftstone.com>
2017-08-15 14:38:33 +0800

    提交到本地仓库

---
+++ SpringBootDemo/src/main/java/org/spring/boot/SpringBootDemo/App.java
@@ -0,0 +1,14 @@
+package org.spring.boot.SpringBootDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World1!" );
+    }
+}
